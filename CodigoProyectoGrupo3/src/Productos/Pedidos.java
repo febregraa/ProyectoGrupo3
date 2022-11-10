6e9948373c72ce56d6665f1fa3417a52688a7998
JRossX <113962486+JRossX@users.noreply.github.com>
2022-11-09 22:07:42 -0600

    Avance Josué
    XD

--- CodigoProyectoGrupo3/src/Productos/Pedidos.java
+++ CodigoProyectoGrupo3/src/Productos/Pedidos.java
@@ -1,5 +1,107 @@
 package Productos;
 
+import javax.swing.JOptionPane;
+
 public class Pedidos {
+
     //Josue
+    private String NombreCliente, ID, Trabajo, Contacto, LugarDeEnvio, TiempoDeEntrega, NumeroDeOrden, Pedido = ""; //Datos del cliente y orden
+    private int CostoDetrabajo, CostoDeEnvio; //Costos
+
+    public void mostrarPedido() {
+        JOptionPane.showMessageDialog(null, Pedido, "Datos de la Orden", 3);
+    }
+
+    public void agregarPedidos() {
+        setNombreCliente(JOptionPane.showInputDialog(null, "Nombre del cliente: ", "Datos de la orden", 3));
+        setID(JOptionPane.showInputDialog(null, "Número de Identidad ", "Datos de la orden", 3));
+        setTrabajo(JOptionPane.showInputDialog(null, "Tipo de trabajo: ", "Datos de la orden", 3));
+        setContacto(JOptionPane.showInputDialog(null, "Número al cual contactar ", "Datos de la orden", 3));
+        setLugarDeEnvio(JOptionPane.showInputDialog(null, "Direccion de entrega: ", "Datos de la orden", 3));
+        setTiempoDeEntrega(JOptionPane.showInputDialog(null, "Tiempo estimado para finalizar el trabajo: ", "Datos de la orden", 3));
+        setNumeroDeOrden(JOptionPane.showInputDialog(null, "Ingrese el número de gestíon: ", "Datos de la orden", 3));
+        setCostoDetrabajo(Integer.parseInt(JOptionPane.showInputDialog(null, "Costo del trabajo: ", "Datos de la orden", 3)));
+        Pedido += ("Nombre del cliente: " + NombreCliente  +"\nID: " + ID + "\nTipo de trabajo: " + Trabajo + "\nNúmero al cual contactar: " + Contacto + "\nDireccion de entrega: " + LugarDeEnvio + "\nTiempo de : " + TiempoDeEntrega + "\nCosto del trabajo: " + CostoDetrabajo + "\nCosto del envio" + CostoDeEnvio + "\n" + "\n");
+
+    }
+
+    public String getNombreCliente() {
+        return NombreCliente;
+    }
+
+    public void setNombreCliente(String NombreCliente) {
+        this.NombreCliente = NombreCliente;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getTrabajo() {
+        return Trabajo;
+    }
+
+    public void setTrabajo(String Trabajo) {
+        this.Trabajo = Trabajo;
+    }
+
+    public String getContacto() {
+        return Contacto;
+    }
+
+    public void setContacto(String Contacto) {
+        this.Contacto = Contacto;
+    }
+
+    public String getLugarDeEnvio() {
+        return LugarDeEnvio;
+    }
+
+    public void setLugarDeEnvio(String LugarDeEnvio) {
+        this.LugarDeEnvio = LugarDeEnvio;
+    }
+
+    public String getTiempoDeEntrega() {
+        return TiempoDeEntrega;
+    }
+
+    public void setTiempoDeEntrega(String TiempoDeEntrega) {
+        this.TiempoDeEntrega = TiempoDeEntrega;
+    }
+
+    public String getNumeroDeOrden() {
+        return NumeroDeOrden;
+    }
+
+    public void setNumeroDeOrden(String NumeroDeOrden) {
+        this.NumeroDeOrden = NumeroDeOrden;
+    }
+
+    public String getPedido() {
+        return Pedido;
+    }
+
+    public void setPedido(String Pedido) {
+        this.Pedido = Pedido;
+    }
+
+    public int getCostoDetrabajo() {
+        return CostoDetrabajo;
+    }
+
+    public void setCostoDetrabajo(int CostoDetrabajo) {
+        this.CostoDetrabajo = CostoDetrabajo;
+    }
+
+    public int getCostoDeEnvio() {
+        return CostoDeEnvio;
+    }
+
+    public void setCostoDeEnvio(int CostoDeEnvio) {
+        this.CostoDeEnvio = CostoDeEnvio;
+    }
 }
--- CodigoProyectoGrupo3/src/Tareas/Trabajos.java
+++ CodigoProyectoGrupo3/src/Tareas/Trabajos.java
@@ -1,5 +1,53 @@
 package Tareas;
 
+import javax.swing.JOptionPane;
+
 public class Trabajos {
+
     //josue
+    private String AsignarTrabajador, TipoDeTrabajo, Prioridad, Trabajo = "";
+
+    public void MostrarTrabajo() {
+        JOptionPane.showMessageDialog(null, Trabajo, "Lista de trabajos por finalizar", 3);
+    }
+
+    public void agregarTrabajoPendiente() {
+        setAsignarTrabajador(JOptionPane.showInputDialog(null, "Indique el nombre del trabajador el cual desea asginar este trabajo: ", "Registro de Trabajos", 3));
+        setTipoDeTrabajo(JOptionPane.showInputDialog(null, "Tipo de trabajo: ", "Registro de Trabajos", 3));
+        setPrioridad(JOptionPane.showInputDialog(null, "Este trabajo necesita Prioridad(Indique SI O NO): ", "Registro de Trabajos", 3));
+        Trabajo += ("Colaborador Asignado: " + AsignarTrabajador + "\nTipo de trabajo a realizar: " + TipoDeTrabajo + "\nPrioridad: " + Prioridad + "\n" + "\n");
+
+    }
+
+    public String getAsignarTrabajador() {
+        return AsignarTrabajador;
+    }
+
+    public void setAsignarTrabajador(String AsignarTrabajador) {
+        this.AsignarTrabajador = AsignarTrabajador;
+    }
+
+    public String getTipoDeTrabajo() {
+        return TipoDeTrabajo;
+    }
+
+    public void setTipoDeTrabajo(String TipoDeTrabajo) {
+        this.TipoDeTrabajo = TipoDeTrabajo;
+    }
+
+    public String getPrioridad() {
+        return Prioridad;
+    }
+
+    public void setPrioridad(String Prioridad) {
+        this.Prioridad = Prioridad;
+    }
+
+    public String getTrabajo() {
+        return Trabajo;
+    }
+
+    public void setTrabajo(String Trabajo) {
+        this.Trabajo = Trabajo;
+    }
 }
